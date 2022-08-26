83834b683d29574ae695091a551a0b196d4caf35
PraneshNagarajan <sathyajana20@gmail.com>
2022-08-26 09:46:32 +0530

    updated ip4 address of backend server

--- src/components/login.js
+++ src/components/login.js
@@ -48,7 +48,7 @@ const LoginPage = () => {
         username: value.username,
         password: value.password,
       };
-      const baseURL = "https://34.226.150.17/auth";
+      const baseURL = "https://34.238.44.9/auth";
       axios
         .post(baseURL, data)
         .then((res) => {
